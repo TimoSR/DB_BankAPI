2df95e10868cd7c021950a172236e51ffdb65b8a
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2024-05-12 15:12:35 +0200

    Adding Fluent Validation

--- transaction_service/src/API/Features/Application/CreateTransactionCommand.cs
+++ transaction_service/src/API/Features/Application/CreateTransactionCommand.cs
@@ -1,11 +1,14 @@
 using System.ComponentModel.DataAnnotations;
-using API.Features.Domain;
 using CodeContracts.Application;
 
 namespace API.Features.Application;
 
 public record CreateTransactionCommand : Command
 {
+    [Required(ErrorMessage = "Account ID is required.")]
     public required string AccountId { get; set; }
+    
+    [Required(ErrorMessage = "Amount is required.")]
+    [NonZeroDecimal]
     public required decimal Amount { get; set; }
 }

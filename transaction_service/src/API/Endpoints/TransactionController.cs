303f874e5fb270bbdbb17176359dcab5e7a344cb
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2024-05-13 11:17:53 +0200

    Adding Transactions Bad requests

--- transaction_service/src/API/Endpoints/TransactionController.cs
+++ transaction_service/src/API/Endpoints/TransactionController.cs
@@ -11,27 +11,51 @@ public class TransactionController(ITransactionService service) : ControllerBase
     public async Task<ActionResult<List<TransactionDTO>>> GetAll()
     {
         var result = await service.GetAllTransactionsAsync();
-        return Ok(result.Data);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Data);
+        }
+
+        return BadRequest(result.Messages);
     }
 
     [HttpGet("GetById")]
     public async Task<ActionResult<TransactionDTO>> GetById(string id)
     {
         var result = await service.GetTransactionByIdAsync(id);
-        return Ok(result.Data);
+        
+        if (result.IsSuccess)
+        {
+            return Ok(result.Data);
+        }
+
+        return BadRequest(result.Messages);
     }
 
     [HttpGet("GetLast10AccountTransaction")]
     public async Task<ActionResult<List<TransactionDTO>>> GetLast10AccountTransaction(string id)
     {
         var result = await service.GetLast10AccountTransactionsAsync(id);
-        return Ok(result.Data);
+        
+        if (result.IsSuccess)
+        {
+            return Ok(result.Data);
+        }
+
+        return BadRequest(result.Messages);
     }
     
     [HttpPost("Create")]
     public async Task<IActionResult> Create(CreateTransactionCommand command)
     {
         var result = await service.CreateTransactionAsync(command);
-        return Ok(result.Messages);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Messages);
+        }
+        
+        return BadRequest(result.Messages);
     }
 }
